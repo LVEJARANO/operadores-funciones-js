b0e2fb5793b226ce385c14456712c7334765bb74
Luis Vejarano <luis.vejarano1.0@gmail.com>
2025-11-18 18:27:56 -0500

    Se agrega ejemplo con elemento input y libreria Math

--- projectOpFunctions/assets/js/funciones.js
+++ projectOpFunctions/assets/js/funciones.js
@@ -5,3 +5,35 @@
 function myFunction(){
     document.getElementById("demo3").innerHTML = "Luis Vejarano";
 }
+
+/**
+ * Funcion para capturar los datos
+ */
+function datos(){
+    let titulo = document.getElementById("titulo").value;
+    let autor = document.getElementById("autor").value;
+    let resultado = unir(titulo, autor);
+    document.getElementById("msj").innerHTML = resultado;
+}
+
+/**
+ * Funcion para unir una cadena de datos
+ */
+function unir(titulo, autor){
+    return "Titulo: " + titulo + "<br/>" 
+    + "Autor: " + autor;
+}
+
+/**
+ * Ejemplo utilizando el objeto Math
+ */
+function redondearNumero(){
+    let num = document.getElementById("numero").value;
+
+    //Convertir a numero y redondear
+    let redondeo = Math.round(Number(num));
+
+    document.getElementById("resultado").innerHTML =
+    "El numero redondeado es: " + redondeo;
+
+}
